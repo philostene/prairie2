564852b94b33c7fb1f5f4ef595f7fc173db736f8
philostene <gspillette@gmail.com>
2017-04-19 17:01:58 +0200

    modifie

--- JavaP1/src/Hello.java
+++ JavaP1/src/Hello.java
@@ -6,7 +6,8 @@ public class Hello {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       System.out.println('hello world');
+      // System.out.println('hello world');
+		public static void dire_bonjour()
 	}
 
 }
